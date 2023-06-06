15dcee12f2566aa5f773ba795b96021e68113304
AndriiHry <andreyhrytcay@gmail.com>
2023-06-06 09:58:16 +0300

    Initial Commit

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by ANDRII HRYTSAI on 06.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
