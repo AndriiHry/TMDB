6063075b4a4e2b668ade60b9ed34aa9e89c65061
AndriiHry <andreyhrytcay@gmail.com>
2023-06-06 11:32:40 +0300

    add FirstViewController

---
+++ TMDB/ViewControllers/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  TMDB
+//
+//  Created by ANDRII HRYTSAI on 06.06.2023.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
